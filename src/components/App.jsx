38f6879de6dc35c483beb8c3848b5bad87dc724c
rayGreycloud <rayGreycloud@gmail.com>
2018-12-31 08:27:47 -0800

    Add language indicator and toggle

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,8 +1,27 @@
 import React, { Component } from 'react';
 
 class App extends Component {
+  state = { language: 'english' };
+
+  onLanguageChange = language => this.setState({ language });
+
   render() {
-    return <div className="ui container">App</div>;
+    return (
+      <div className="ui container">
+        <div>
+          Select a language:
+          <i
+            className="flag us"
+            onClick={() => this.onLanguageChange('english')}
+          />
+          <i
+            className="flag mx"
+            onClick={() => this.onLanguageChange('spanish')}
+          />
+        </div>
+        {this.state.language}
+      </div>
+    );
   }
 }
 
