ece548e3380429ea5a55de89c4502e7fd2202327
rayGreycloud <rayGreycloud@gmail.com>
2018-12-31 12:43:26 -0800

    Wire up Provider for language context

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 
 import UserCreate from './UserCreate';
+import LanguageContext from '../contexts/LanguageContext';
 
 class App extends Component {
   state = { language: 'english' };
@@ -21,7 +22,9 @@ class App extends Component {
             onClick={() => this.onLanguageChange('spanish')}
           />
         </div>
-        <UserCreate />
+        <LanguageContext.Provider value={this.state.language}>
+          <UserCreate />
+        </LanguageContext.Provider>
       </div>
     );
   }
