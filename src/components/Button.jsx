a87c197237de14c9e8b9ed1a8ea72a8a849a3fbb
rayGreycloud <rayGreycloud@gmail.com>
2018-12-31 11:57:05 -0800

    get language context value

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,9 +1,19 @@
 import React, { Component } from 'react';
 
+import LanguageContext from '../contexts/LanguageContext';
+
 class Button extends Component {
+  // prescribed variable name
+  static contextType = LanguageContext;
+
   render() {
-    return <button className="ui button primary">Submit</button>;
+    const text = this.context === 'english' ? 'Submit' : 'Enviar';
+
+    return <button className="ui button primary">{text}</button>;
   }
 }
+
+// alternative method to set context type
+// Button.contextType = LanguageContext;
 
 export default Button;
--- src/components/Field.jsx
+++ src/components/Field.jsx
@@ -1,10 +1,16 @@
 import React, { Component } from 'react';
 
+import LanguageContext from '../contexts/LanguageContext';
+
 class Field extends Component {
+  static contextType = LanguageContext;
+
   render() {
+    const text = this.context === 'english' ? 'Name' : 'Nombre';
+
     return (
       <div className="ui field">
-        <label htmlFor="name">Name </label>
+        <label htmlFor="name">{text} </label>
         <input type="text" style={{ maxWidth: '300px' }} />
       </div>
     );
